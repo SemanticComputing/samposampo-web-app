0d95d5d217ecb6d15b703bbbcb32d2f899d0eee6
Petri Leskinen <petri.leskinen@aalto.fi>
2025-02-06 13:32:52 +0200

    Add fields to person pages

--- src/server/sparql/samposampo/sparql_queries/SparqlQueriesPeople.js
+++ src/server/sparql/samposampo/sparql_queries/SparqlQueriesPeople.js
@@ -9,4 +9,36 @@ export const personProperties = `
     BIND(?id as ?uri__dataProviderUrl)
     BIND(?id as ?uri__prefLabel)
   }
+  UNION
+  { ?proxy foaf:focus ?id
+    {
+      ?proxy sch:gender/skos:prefLabel ?gender
+    }
+    UNION
+    {
+      ?proxy sampos:birth_time/skos:prefLabel ?birth_time
+    }
+    UNION
+    {
+      ?proxy sch:birthPlace ?birth_place__id .
+      ?birth_place__id skos:prefLabel ?birth_place__prefLabel .
+      BIND (CONCAT("/places/page/", REPLACE(STR(?birth_place__id), "^.*\\\\/(.+)", "$1")) AS ?birth_place__dataProviderUrl)
+    }
+    UNION
+    {
+      ?proxy sampos:death_time/skos:prefLabel ?death_time
+    }
+    UNION
+    {
+      ?proxy sch:deathPlace ?death_place__id .
+      ?death_place__id skos:prefLabel ?death_place__prefLabel .
+      BIND (CONCAT("/places/page/", REPLACE(STR(?death_place__id), "^.*\\\\/(.+)", "$1")) AS ?death_place__dataProviderUrl)
+    }
+    UNION
+    {
+      ?proxy owl:sameAs|foaf:page ?external__id .
+      ?external__id a/skos:prefLabel ?external__prefLabel .
+      BIND (?external__id as ?external__dataProviderUrl)
+    }
+  }
 `
