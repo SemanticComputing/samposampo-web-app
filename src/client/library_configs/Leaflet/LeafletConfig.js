e7bb998fa0ee208453127d16623a0fdd753a8972
Annastiina Ahola <anna.s.ahola@gmail.com>
2025-02-19 11:28:36 +0200

    Add check for empty related actor list in popup

--- src/client/library_configs/Leaflet/LeafletConfig.js
+++ src/client/library_configs/Leaflet/LeafletConfig.js
@@ -69,7 +69,7 @@ export const createPopUpContentLetterSampo = ({ data, resultClass }) => {
     h3.textContent = data.prefLabel.prefLabel
   }
   container.appendChild(h3)
-  if (resultClass === 'placesActors' || resultClass === 'placesLocations') {
+  if (resultClass === 'placesActors' || (resultClass === 'placesLocations' && data.related)) {
     const p = document.createElement('p')
     p.textContent = 'Actors:'
     container.appendChild(p)
