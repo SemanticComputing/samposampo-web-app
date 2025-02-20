6fa439ea7ff9a321a9b5650f28a173dfb1fe4430
Annastiina Ahola <anna.s.ahola@gmail.com>
2025-02-20 17:07:40 +0200

    Add basic place proxy pages

---
+++ src/server/sparql/samposampo/sparql_queries/SparqlQueriesPlaceProxies.js
@@ -0,0 +1,28 @@
+const perspectiveID = 'place_proxies'
+
+export const placeProxyProperties = `
+  {
+    ?id skos:prefLabel ?prefLabel__id .
+    ?id dce:source ?source .
+    BIND(CONCAT(?prefLabel__id, ' (in ', ?source, ')') as ?prefLabel__prefLabel)
+    BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+    BIND(?id as ?uri__id)
+    BIND(?id as ?uri__dataProviderUrl)
+    BIND(?id as ?uri__prefLabel)
+  }
+  UNION {
+    ?id foaf:focus ?provided__id .
+    ?provided__id skos:prefLabel ?provided__prefLabel .
+    FILTER(LANG(?provided__prefLabel) = 'fi')
+    BIND(CONCAT("/places/page/", REPLACE(STR(?provided__id), "^.*\\\\/(.+)", "$1")) AS ?provided__dataProviderUrl)
+  }
+  UNION {
+    ?id skos:altLabel ?altLabel .
+  }
+  UNION {
+    ?id wgs84:lat ?latitude .
+  }
+  UNION {
+    ?id wgs84:long ?longitude .
+  }
+`
