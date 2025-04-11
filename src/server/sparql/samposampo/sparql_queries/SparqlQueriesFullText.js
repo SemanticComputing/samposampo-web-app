d33e8a2a98c82a6570802d20489b8dc6c40c1477
Petri Leskinen <petri.leskinen@aalto.fi>
2025-04-11 13:17:37 +0300

    Simplify fulltextquery

--- src/server/sparql/samposampo/sparql_queries/SparqlQueriesFullText.js
+++ src/server/sparql/samposampo/sparql_queries/SparqlQueriesFullText.js
@@ -1,5 +1,5 @@
 export const fullTextSearchProperties = `
-VALUES (?type__id ?type__label ?pagetype)
+VALUES (?type__id ?type__prefLabel ?pagetype)
 {
   (sampos:PersonProxy "Person" "/people")
   (sampos:OrganizationProxy "Organization" "/groups")
@@ -10,18 +10,14 @@ VALUES (?type__id ?type__label ?pagetype)
 OPTIONAL { ?id foaf:focus ?provided }
 BIND (COALESCE(?provided, ?id) AS ?match)
 
-BIND (COALESCE(?type__class, ?type__label) AS ?type__prefLabel)
-
 ?match skos:prefLabel ?prefLabel__id .
 BIND(?prefLabel__id as ?prefLabel__prefLabel)
 BIND(CONCAT(?pagetype, "/page/", REPLACE(STR(?match), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
 
 OPTIONAL {
-  ?match ^foaf:focus? [
-        sch:image ?image__id ;
+  ?match sch:image ?image__id ;
     	skos:prefLabel ?image__description ;
-    	skos:prefLabel ?image__title 
-    ]
+    	skos:prefLabel ?image__title .
   BIND(CONCAT(REPLACE(STR(?image__id), "https*:", ""), "?width=40") as ?image__url)
 }
 `
