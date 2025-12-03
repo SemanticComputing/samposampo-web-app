31cba7c63c653127b32017a1a16bf36a8a92bcac
Petri Leskinen <petri.leskinen@aalto.fi>
2025-12-03 15:51:49 +0200

    Add map tab to Orgazations

--- src/server/sparql/samposampo/sparql_queries/SparqlQueriesGroups.js
+++ src/server/sparql/samposampo/sparql_queries/SparqlQueriesGroups.js
@@ -115,114 +115,36 @@ export const groupPropertiesInstancePage = `
   }
 `
 
-export const peopleMapQuery = `
+export const groupsMapQuery = `
 SELECT DISTINCT ?id ?lat ?long 
-(COUNT(DISTINCT ?person) AS ?instanceCount)
+(COUNT(DISTINCT ?group) AS ?instanceCount)
 WHERE {
   <FILTER>
-  { 
-    [] foaf:focus ?person ;
-      sch:birthPlace ?id 
-  }
-  UNION
-  { 
-    [] foaf:focus ?person ;
-      sch:deathPlace ?id 
-  }
 
-[] foaf:focus ?id ;
-   wgs84:lat ?lat ; wgs84:long ?long .
+  [] foaf:focus ?group ;
+     sch:location ?id .
+  
+  ?id wgs84:lat ?lat ; wgs84:long ?long .
 
 } GROUP BY ?id ?lat ?long
 `
 
 export const placePropertiesInfoWindow = `
   OPTIONAL { ?id skos:prefLabel ?_label }
+  FILTER (LANG(?_label) = "en")
   BIND (COALESCE(?_label, "<place>") AS ?prefLabel__id)
   BIND (?prefLabel__id AS ?prefLabel__prefLabel)
   BIND (CONCAT("/places/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
 `
 
-export const peopleRelatedTo = `
+export const groupsRelatedTo = `
   OPTIONAL {
     <FILTER>
     
-    { [] foaf:focus ?related__id ;
-      sch:birthPlace ?id
-    }
-    UNION
-    { [] foaf:focus ?related__id ;
-      sch:deathPlace ?id
-    }
+    [] foaf:focus ?related__id ;
+       sch:location ?id .
+    
     ?related__id skos:prefLabel ?related__prefLabel .
-    BIND (CONCAT("/people/page/", REPLACE(STR(?related__id), "^.*\\\\/(.+)", "$1")) AS ?related__dataProviderUrl)
+    BIND (CONCAT("/${perspectiveID}/page/", REPLACE(STR(?related__id), "^.*\\\\/(.+)", "$1")) AS ?related__dataProviderUrl)
   }
 `
-
-export const ageQuery = ` SELECT ?category (count(DISTINCT ?born) AS ?Births) (count(DISTINCT ?deceased) AS ?Deaths)
-WHERE {
-  
-  { SELECT DISTINCT ?person__id (SAMPLE(?_proxy) AS ?proxy) WHERE {
-      <FILTER>
-
-	  	?person__id a sch:Person .
-      ?_proxy foaf:focus ?person__id ; sampos:birth_time/time:hasBeginning [] .
-    } GROUPBY ?person__id }
-  FILTER (BOUND(?proxy))
-  {
-    ?proxy sampos:birth_time/time:hasBeginning ?time .
-    BIND (?proxy AS ?born)
-  }
-  UNION
-  {
-    ?proxy  sampos:death_time/time:hasEnd ?time .
-    BIND (?proxy AS ?deceased)
-  }
-  
-  BIND (STR(year(?time)) AS ?category)
-  FILTER (?category <= STR(YEAR(NOW())))
-} GROUPBY ?category ORDER BY ?category
-`
-
-export const peopleMigrationsQuery = `
-SELECT DISTINCT ?id 
-?from__id ?from__prefLabel ?from__lat ?from__long
-?to__id ?to__prefLabel ?to__lat ?to__long
-(CONCAT("/places/page/", REPLACE(STR(?from__id), "^.*\\\\/(.+)", "$1")) AS ?from__dataProviderUrl)
-(CONCAT("/places/page/", REPLACE(STR(?to__id), "^.*\\\\/(.+)", "$1")) AS ?to__dataProviderUrl)
-(COUNT(DISTINCT ?person) as ?instanceCount)
-WHERE {
-  <FILTER>
-  ?person a sch:Person ;
-    ^foaf:focus [ sch:birthPlace ?from__id ;
-                sch:deathPlace ?to__id ] .
-  
-  FILTER(?from__id != ?to__id)
-
-  ?from__id skos:prefLabel ?from__prefLabel .
-  FILTER (lang(?from__prefLabel)="fi")
-  
-  { SELECT DISTINCT ?from__id (SAMPLE(?_from_proxy) AS ?from_proxy) 
-      WHERE { ?_from_proxy foaf:focus ?from__id ; 
-    			wgs84:lat [] } 
-    GROUPBY ?from__id }
-  ?from_proxy wgs84:lat ?from__lat ; wgs84:long ?from__long .
-  
-  
-  ?to__id skos:prefLabel ?to__prefLabel .
-  FILTER (lang(?to__prefLabel)="fi")
-  { SELECT DISTINCT ?to__id (SAMPLE(?_to_proxy) AS ?to_proxy) 
-      WHERE { ?_to_proxy foaf:focus ?to__id ; 
-    			wgs84:lat [] }
-    GROUPBY ?to__id }
-  
-  ?to_proxy wgs84:lat ?to__lat ; wgs84:long ?to__long .
-
-  BIND (IRI(CONCAT(STR(?from__id), "-", REPLACE(STR(?to__id), "^.*\\\\/(.+)", "$1"))) as ?id)
-}
-GROUP BY ?id 
-?from__id ?from__prefLabel ?from__lat ?from__long
-?to__id ?to__prefLabel ?to__lat ?to__long
-ORDER BY desc(?instanceCount)
-`
-
