a6f11dc6459b512240d2ec1b534868705a61b633
Petri Leskinen <petri.leskinen@aalto.fi>
2025-12-03 21:56:38 +0200

    Finetune full-text-search

--- src/server/sparql/JenaQuery.js
+++ src/server/sparql/JenaQuery.js
@@ -1,5 +1,6 @@
 import { runSelectQuery } from './SparqlApi'
-import { fullTextQuery } from './SparqlQueriesGeneral'
+// import { fullTextQuery } from './SparqlQueriesGeneral'
+import { fullTextQuery } from './samposampo/sparql_queries/SparqlQueriesFullText'
 import { makeObjectList } from './Mappers'
 
 export const queryJenaIndex = async ({
@@ -11,7 +12,7 @@ export const queryJenaIndex = async ({
   let q = fullTextQuery
   const perspectiveConfig = backendSearchConfig[resultClass]
   const { endpoint, propertiesQueryBlock } = perspectiveConfig
-  q = q.replace('<QUERY>', `(?id ?score) text:query ('${queryTerm.toLowerCase()}' 2000) .`)
+  q = q.replace('<QUERY>', `(?proxy ?score) text:query ('${queryTerm.toLowerCase()}' 25000) .`)
   q = q.replace('<RESULT_SET_PROPERTIES>', propertiesQueryBlock)
   const results = await runSelectQuery({
     query: endpoint.prefixes + q,
--- src/server/sparql/samposampo/sparql_queries/SparqlQueriesFullText.js
+++ src/server/sparql/samposampo/sparql_queries/SparqlQueriesFullText.js
@@ -6,18 +6,28 @@ VALUES (?type__id ?type__prefLabel ?pagetype)
   (sampos:PlaceProxy "Place" "/places")
 }
 
-?id a ?type__id .
-OPTIONAL { ?id foaf:focus ?provided }
-BIND (COALESCE(?provided, ?id) AS ?match)
+?proxy a ?type__id ; foaf:focus ?id .
 
-?match skos:prefLabel ?prefLabel__id .
+?id skos:prefLabel ?prefLabel__id .
 BIND(?prefLabel__id as ?prefLabel__prefLabel)
-BIND(CONCAT(?pagetype, "/page/", REPLACE(STR(?match), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+BIND(CONCAT(?pagetype, "/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
 
 OPTIONAL {
-  ?match sch:image ?image__id ;
+  ?id sch:image ?image__id ;
     	skos:prefLabel ?image__description ;
     	skos:prefLabel ?image__title .
   BIND(CONCAT(REPLACE(STR(?image__id), "https*:", ""), "?width=40") as ?image__url)
 }
+
+OPTIONAL { ?id sampos:pagelinks ?pagelinks }
+`
+
+export const fullTextQuery = `
+  SELECT DISTINCT ?id ?prefLabel__id ?prefLabel__prefLabel ?prefLabel__dataProviderUrl ?type__id ?type__prefLabel 
+   ?image__id ?image__description ?image__title ?image__url
+  WHERE {
+    <QUERY>
+    <RESULT_SET_PROPERTIES>
+  }
+  ORDER BY DESC(?score) DESC(COALESCE(?pagelinks, 5)) STR(?prefLabel__id)
 `
