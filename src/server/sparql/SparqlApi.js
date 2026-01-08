6de55e1a831b41db8981b66b9b73becd3181583d
Annastiina Ahola <anna.s.ahola@gmail.com>
2026-01-08 18:50:15 +0200

    Comment out a console.log command

--- src/server/sparql/SparqlApi.js
+++ src/server/sparql/SparqlApi.js
@@ -21,7 +21,7 @@ export const runSelectQuery = async ({
   if (useAuth) {
     headers.Authorization = `Basic ${process.env.SPARQL_ENDPOINT_BASIC_AUTH}`
   }
-  console.log(query)
+  // console.log(query)
   const q = querystring.stringify({ query })
   try {
     const response = await axios({
